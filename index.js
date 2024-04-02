de9437fc548c9bcc2d4b465de0f365f6bfa30de1
Lovertab <lovertabrown@gmail.com>
2024-04-01 21:05:37 -0400

    added color prompt

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ inquirer
             type: "input",
             message: "enter three character:",
             name: "text",
+        },
+        {
+            type: "input",
+            message: "Enter a text color (it can be keyword or a hexadecimal number):",
+            name:"color",
         }
     ])
     .then((response) => {
         console.log(response);
         console.log(`The three characters you enter are: ${response.text}!`);
+        console.log(`The color you chose is: ${response.color}!`);
     }
     );
