8617d0e8168da56cd8f02a84c15fb5a95102b077
Lovertab <lovertabrown@gmail.com>
2024-04-01 21:16:13 -0400

    added shape color prompt

--- index.js
+++ index.js
@@ -9,19 +9,25 @@ inquirer
         {
             type: "input",
             message: "Enter a text color (it can be keyword or a hexadecimal number):",
-            name:"color",
+            name: "color",
         },
         {
             type: "list",
             message: "What shape do you want?",
             name: "shapes",
-            choices: ["circle","triangle","square","star"],
+            choices: ["circle", "triangle", "square", "star"],
+        },
+        {
+            type: "input",
+            message: "Enter a shape color (it can be keyword or a hexadecimal number):",
+            name: "shapeColor",
         }
     ])
     .then((response) => {
         console.log(response);
         console.log(`The three characters you enter are: ${response.text}!`);
-        console.log(`The color you chose is: ${response.color}!`);
+        console.log(`The text color you chose is: ${response.color}!`);
         console.log(`The shape you chose is: ${response.shapes}!`);
+        console.log(`The shape color you chose is: ${response.shapeColor}!`);
     }
     );
